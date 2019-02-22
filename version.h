7ad5a1bdb48eca3799a17946c12a1e2e8fe5cc73
EVGESHAd <evgeniy.d@informmonitoring.ru>
2019-02-22 10:21:57 +0300

    ops

---
+++ version.h
@@ -0,0 +1,33 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+namespace AutoVersion{
+	
+	//Date Version Types
+	#define DATE "22"
+	#define MONTH "02"
+	#define YEAR "2019"
+	#define UBUNTU_VERSION_STYLE  "19.02"
+	
+	//Software Status
+	#define STATUS  "Alpha"
+	#define STATUS_SHORT  "a"
+	
+	//Standard Version Type
+	#define MAJOR  1
+	#define MINOR  0
+	#define BUILD  1
+	#define REVISION  4
+	
+	//Miscellaneous Version Types
+	#define BUILDS_COUNT  13
+	#define RC_FILEVERSION 1,0,1,4
+	#define RC_FILEVERSION_STRING "1, 0, 1, 4\0"
+	#define FULLVERSION_STRING  "1.0.1.4"
+	
+	//These values are to keep track of your versioning state, don't modify them.
+	#define BUILD_HISTORY  1
+	
+
+}
+#endif //VERSION_H
